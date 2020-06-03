501941b874f8d7db98bed8d41f5306cf1588a247
Thomas Snellgrove <tsnellgrove@hotmail.com>
2020-06-02 21:23:23 -0400

    Flask test files added to git

---
+++ flask_hello_world.py
@@ -0,0 +1,92 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template, request, session
+
+from processing import do_calculation
+
+# create the application object
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "qpueuwrhuqjfn;nWOREJ"
+
+# use decorators to link the function to a url
+@app.route('/', methods=["GET", "POST"])
+
+#def home():
+#    return "Hello, Big World!"  # return a string
+
+@app.route('/')
+def index():
+
+    if "count" in session:
+        session["count"] = session.get("count") + 1
+    else:
+        session["count"] = 1
+    if "test_lst" not in session:
+        session["test_lst"] = []
+    if "exit_now" not in session:
+        session["exit_now"] = False
+    if "text1" not in session:
+        session["text1"] = "blank"
+
+    if request.method == "POST":
+        session["text1"] = str(request.form["text1"])
+        if session["exit_now"]:
+            session.pop("text1", None)
+            session.pop('count', None)
+            session['test_lst'] = []
+            session.pop('exit_now', None)
+            print("Session Popped")
+
+    if 'text1' in session:
+        session["buffer_txt"], session["exit_now"], session["test_lst"] = do_calculation(session['text1'], session["test_lst"])
+        session.modified = True
+        print(session["exit_now"])
+        print(session["test_lst"])
+        print(session["count"])
+
+    return render_template('index.html', output = session["buffer_txt"], my_list = session["test_lst"])
+
+
+@app.route('/readme')
+def dark_castle():
+    return render_template('readme.html')  # render a template
+
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+#    app.run(debug=True)
+    app.run(use_reloader=False, debug=True)
+
+
+#	*** Pseudo Code ***
+
+#	HTML Template in /Templates 
+#		TITLE "Dark Castle"
+#		Print {{buffer}}
+#		Form to accept input 'player_command'
+
+#	Python Code - Dark Castle Program
+#		Contains all functions
+#		Contains all static dict
+#		All print() => 'buffer' via redirect of stdio
+#		All end conditions => return 'game_reset' = True
+#		Text interpreter function
+#			Implements 'quit'
+#			Contains timer code
+#			Returns: 'buffer', 'game_reset', <all stateful dicts>
+
+#	Flask App
+#		Standard opening & closing
+#		Route for /
+#		Local variables
+#			if 'game_reset' not in session: session['game_reset'] = True
+#			if session['game_reset'] == True: set 'player_command' to "game_start_xyz" & initialize all stateful dicts
+#			session['game_reset'] = False
+#		Get input
+#			if request.method == 'POST': session['player_command'] = <player_command> (???)
+#		Call python app
+#			buffer, game_reset, <stateful dicts> = text_interpreter('player_command', <stateful dicts>)
+#		return render_template(output = session['buffer'])
+
+#	Consider adding a clean up routine that runs if the player hits a separate 'quit & close out' button
+#	Move flask docs to git and branch before implementing pseudo code test
+
---
+++ processing.py
@@ -0,0 +1,34 @@
+test_lst = []
+
+def func_test(base):
+	full = base + " Burt"
+	return full
+
+def do_calculation(color, test_lst):
+	exit_now = False
+	color = color.lower()
+	if color == 'red':
+		buffer = 'like an apple'
+	elif color == 'yellow':
+		buffer = 'like the sun'
+	elif color == 'blue':
+		buffer= 'like the ocean'
+	elif color == 'blank':
+		buffer = "Enter a primary color"
+	elif color == 'quit':
+		buffer = 'goodbye!'
+		exit_now = True
+	else:
+		buffer = 'what was that?'
+	buffer = func_test(buffer)
+	test_lst.append(color)
+	return buffer, exit_now, test_lst
+
+#test_string = ['red', 'yellow', 'blue', 'xxyyz', 'quit']
+
+#for value in test_string:
+#	buffer, exit_now, test_lst = do_calculation(value, test_lst)
+#	print("The test response to " + value + " is {}".format(buffer))
+#	print("The exit_now value is " + str(exit_now))
+#	print(test_lst)
+
