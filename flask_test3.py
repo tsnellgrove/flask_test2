89de811d996b35dd80336a307519e0c89cda7129
Thomas Snellgrove <tsnellgrove@hotmail.com>
2020-07-08 19:41:51 -0400

    minor updates

--- flask_test3.py
+++ flask_test3.py
@@ -70,6 +70,7 @@ if __name__ == '__main__':
 # July 4: Signed up for a pythonanywhere account today to try a differet web server
 # July 5: Pushed flask_test2 to GitHub and then manually cloned it to my pythonanywhere account
 # July 7: Started working on basch on pythonanywhere
+# July 8: Yet more pythonanywhere troubleshooting
 
 # for testing
 #    session.pop('game_over', None)
